0f4ebc29fbe74227a7b7e4b33364157fbb19ee12
Asif Rehan <asif.rehan9@gmail.com>
2020-09-16 01:51:15 -0400

    wip: model comparison test

--- supervised_learning/src/experiments.py
+++ supervised_learning/src/experiments.py
@@ -81,21 +81,20 @@ def plot_learning_curve(estimator, title, X, y, axes=None, ylim=None, cv=None,
     return plt
 
 
-
 def vanilla_fit(X_train, y_train, pipe):
     pipe.fit(X_train, y_train)
     train_score = pipe.score(X_train, y_train)
     print(train_score)
-    #print(accuracy_score(y_test, pipe.predict(X_test)))
+    # print(accuracy_score(y_test, pipe.predict(X_test)))
     return train_score
 
 
-def generate_validation_curve(pipe, X_train, y_train, model, param_name, search_range,   data='wine'):
-    train_scores, test_scores = validation_curve(estimator=pipe, X=X_train, y=y_train, 
-                                                 param_name="cfr__"+param_name,
+def generate_validation_curve(pipe, X_train, y_train, model, param_name, search_range, data='wine'):
+    train_scores, test_scores = validation_curve(estimator=pipe, X=X_train, y=y_train,
+                                                 param_name="cfr__" + param_name,
                                                  param_range=search_range, cv=5,
                                                  n_jobs=-1)
-    plt.figure(figsize=(5,5))
+    plt.figure(figsize=(5, 5))
     plt.plot(search_range, np.mean(train_scores, axis=1), label='Training score')
     plt.plot(search_range, np.mean(test_scores, axis=1), label='Cross-validation score')
     plt.title('Validation curve for{}'.format(model))
@@ -106,6 +105,7 @@ def generate_validation_curve(pipe, X_train, y_train, model, param_name, search_
 
     plt.savefig(r'..\plot\{}_val_curve_{}_{}.png'.format(data, model, param_name), figsize=(5, 5))
     plt.show()
+
     
 def tune_hyperparameter(param_grid, pipe, X_train, y_train):
     cv = StratifiedKFold(n_splits=5, random_state=42)
@@ -117,128 +117,127 @@ def tune_hyperparameter(param_grid, pipe, X_train, y_train):
     print(classification_report(y_train, ypred))
     return tuned_model
 
+
 def decision_tree_experiments(X_train, y_train, data='wine'):
     pipe = Pipeline([('std', StandardScaler()), ('cfr', DecisionTreeClassifier())])
     vanilla_fit(X_train, y_train, pipe)
-    
     
     min_samples_split_range = np.arange(2, 300, 10)
     min_samples_leaf_range = np.arange(1, 160, 10)
     ccp_alpha_range = np.linspace(0, 0.1, 10)
     max_depth_range = np.arange(2, 25, 1)
     
-    generate_validation_curve(pipe, X_train, y_train, model='Decision Tree', 
-                              param_name="min_samples_leaf", 
+    generate_validation_curve(pipe, X_train, y_train, model='Decision Tree',
+                              param_name="min_samples_leaf",
                               search_range=min_samples_leaf_range, data=data)
-    generate_validation_curve(pipe, X_train, y_train, model='Decision Tree', 
-                              param_name="min_samples_split", 
+    generate_validation_curve(pipe, X_train, y_train, model='Decision Tree',
+                              param_name="min_samples_split",
                               search_range=min_samples_split_range , data=data)    
-    generate_validation_curve(pipe, X_train, y_train, model='Decision Tree', 
-                          param_name="ccp_alpha", 
+    generate_validation_curve(pipe, X_train, y_train, model='Decision Tree',
+                          param_name="ccp_alpha",
                           search_range=ccp_alpha_range , data=data)    
-    generate_validation_curve(pipe, X_train, y_train, model='Decision Tree', 
-                          param_name="max_depth", 
+    generate_validation_curve(pipe, X_train, y_train, model='Decision Tree',
+                          param_name="max_depth",
                           search_range=max_depth_range , data=data)    
     
-    tuned_model = tune_hyperparameter({'cfr__min_samples_split': min_samples_split_range, 
+    tuned_model = tune_hyperparameter({'cfr__min_samples_split': min_samples_split_range,
                                           "cfr__min_samples_leaf": min_samples_leaf_range,
                                           "cfr__ccp_alpha": ccp_alpha_range,
                                           "cfr__max_depth": max_depth_range
-                                          }, 
+                                          },
                                        pipe, X_train, y_train)
     return tuned_model    
 
+
 def knn_experiments(X_train, y_train, data='wine'):
-    pipe =  Pipeline([('std', StandardScaler()), ('cfr', KNeighborsClassifier())])
+    pipe = Pipeline([('std', StandardScaler()), ('cfr', KNeighborsClassifier())])
     vanilla_fit(X_train, y_train, pipe)
-    
     
     n_neighbors_range = np.arange(1, 50, 5)
     p_range = np.linspace(1, 3, 8)
     weights = ['uniform', 'distance']
-    generate_validation_curve(pipe, X_train, y_train, model='kNN', 
-                              param_name="n_neighbors", 
+    generate_validation_curve(pipe, X_train, y_train, model='kNN',
+                              param_name="n_neighbors",
                               search_range=n_neighbors_range, data=data)
-    generate_validation_curve(pipe, X_train, y_train, model='kNN', 
-                              param_name="p", 
+    generate_validation_curve(pipe, X_train, y_train, model='kNN',
+                              param_name="p",
                               search_range=p_range , data=data)    
-    generate_validation_curve(pipe, X_train, y_train, model='kNN', 
-                          param_name="weights", 
+    generate_validation_curve(pipe, X_train, y_train, model='kNN',
+                          param_name="weights",
                           search_range=weights , data=data)    
 
     tuned_model = tune_hyperparameter({'cfr__n_neighbors': n_neighbors_range,
                                       'cfr__weights': weights,
                                       'cfr__p': p_range
-                                      }, 
+                                      },
                                        pipe, X_train, y_train)
     return tuned_model 
 
+
 def neural_network_experiments(X_train, y_train, data='wine'):
-    pipe =  Pipeline([('std', StandardScaler()), ('cfr', MLPClassifier())])
+    pipe = Pipeline([('std', StandardScaler()), ('cfr', MLPClassifier())])
     vanilla_fit(X_train, y_train, pipe)
-    
     
     activation = ['identity', 'logistic', 'tanh', 'relu']
     alpha = np.logspace(-4, 4, 9)
     
-    generate_validation_curve(pipe, X_train, y_train, model='Neural Network', 
-                              param_name="activation", 
+    generate_validation_curve(pipe, X_train, y_train, model='Neural Network',
+                              param_name="activation",
                               search_range=activation, data=data)
-    generate_validation_curve(pipe, X_train, y_train, model='Neural Network', 
-                              param_name="alpha", 
+    generate_validation_curve(pipe, X_train, y_train, model='Neural Network',
+                              param_name="alpha",
                               search_range=alpha, data=data)
     tuned_model = tune_hyperparameter({'cfr__alpha': alpha,
-                                       }, 
+                                       },
                                        pipe, X_train, y_train)
     return tuned_model    
 
+
 def support_vector_machine_experiments(X_train, y_train, data='wine'):
-    pipe =  Pipeline([('std', StandardScaler()), ('cfr', SVC())])
+    pipe = Pipeline([('std', StandardScaler()), ('cfr', SVC())])
     vanilla_fit(X_train, y_train, pipe)
     
     C_range = np.logspace(-2, 3, 6)
     gamma_range = np.logspace(-6, -1, 5)
     kernel_options = ['linear', 'poly', 'rbf', 'sigmoid']
     
-    generate_validation_curve(pipe, X_train, y_train, model='Support Vector Machine', 
-                              param_name="C", 
+    generate_validation_curve(pipe, X_train, y_train, model='Support Vector Machine',
+                              param_name="C",
                               search_range=C_range, data=data)
-    generate_validation_curve(pipe, X_train, y_train, model='Support Vector Machine', 
-                                  param_name="gamma", 
+    generate_validation_curve(pipe, X_train, y_train, model='Support Vector Machine',
+                                  param_name="gamma",
                                   search_range=gamma_range, data=data)    
-    generate_validation_curve(pipe, X_train, y_train, model='Support Vector Machine', 
-                                  param_name="kernel", 
+    generate_validation_curve(pipe, X_train, y_train, model='Support Vector Machine',
+                                  param_name="kernel",
                                   search_range=kernel_options, data=data)    
-    
     
     tuned_model = tune_hyperparameter({'cfr__C': C_range,
                                       'cfr__kernel': kernel_options,
-                                      'cfr__gamma': gamma_range}, 
+                                      'cfr__gamma': gamma_range},
                                        pipe, X_train, y_train)
     return tuned_model    
 
+
 def boosted_tree_experiments(base, X_train, y_train, data='wine'):
-    pipe =  Pipeline([('std', StandardScaler()), 
+    pipe = Pipeline([('std', StandardScaler()),
                  ('cfr', AdaBoostClassifier(base_estimator=base))])
     vanilla_fit(X_train, y_train, pipe)
     
-    n_estimators_range = np.arange(1,1000,50)
+    n_estimators_range = np.arange(1, 1000, 50)
     learning_rate_range = np.linspace(0.01, 1, 20)
     
-    generate_validation_curve(pipe, X_train, y_train, model='Boosted Tree', 
-                              param_name="n_estimators", 
+    generate_validation_curve(pipe, X_train, y_train, model='Boosted Tree',
+                              param_name="n_estimators",
                               search_range=n_estimators_range, data=data)
-    generate_validation_curve(pipe, X_train, y_train, model='Boosted Tree', 
-                              param_name="learning_rate", 
+    generate_validation_curve(pipe, X_train, y_train, model='Boosted Tree',
+                              param_name="learning_rate",
                               search_range=learning_rate_range, data=data)
     
-    
     tuned_model = tune_hyperparameter({'cfr__learning_rate': learning_rate_range,
-              'cfr__n_estimators': np.arange(1,1000,50)
-              }, 
+              'cfr__n_estimators': np.arange(1, 1000, 50)
+              },
                                        pipe, X_train, y_train)
     return tuned_model    
-
 
 
 def get_dataset1(verbose=True):
@@ -246,7 +245,7 @@ def get_dataset1(verbose=True):
     y1 = df['quality'] >= 7
     X1 = df.drop(columns='quality')
     
-    if verbose==True:
+    if verbose == True:
         print(df.shape)
         print(df.info())
         print(df.shape)
@@ -266,20 +265,23 @@ def get_dataset1(verbose=True):
 
     return X1, y1
 
+
 def get_dataset2():
     X2 = None
     y2 = None
     return X2, y2
 
-def test_score(*pipelines):
-    pipe.score(X_test, y_test)
+
+def test_score(result_df, X_train, y_train, X_test, y_test):
+    for i in result_df:
+        pipe = result_df 
+        pipe.score(X_test, y_test)
+
 
 def main(verbose):
     
     X1, y1 = get_dataset1(verbose)
     X2, y2 = get_dataset2()
-    
-    
     
     X_train1, X_test1, y_train1, y_test1 = train_test_split(X1, y1, test_size=0.3, random_state=42)
     X_train2, X_test2, y_train2, y_test2 = train_test_split(X2, y2, test_size=0.3, random_state=42)
@@ -293,59 +295,66 @@ def main(verbose):
     '''
     tuned_decision_tree_model_wine = decision_tree_experiments(X_train1, y_train1, data='wine')
     tuned_decision_tree_model_dataset2 = decision_tree_experiments(X_train1, y_train1, data='dataset2')
-    generate_learning_curves(tuned_decision_tree_model_wine, 
+    generate_learning_curves(tuned_decision_tree_model_wine,
                              tuned_decision_tree_model_dataset2,
                              X_train1, y_train1, X_train2, y_train2)
     
     tuned_boosted_tree_model_wine = boosted_tree_experiments(tuned_decision_tree_model_wine.best_estimator_.get_params()['cfr'],
                                                              X_train1, y_train1, data='wine')
     tuned_boosted_tree_model_dataset2 = boosted_tree_experiments(tuned_decision_tree_model_dataset2.best_estimator_.get_params()['cfr'], X_train1, y_train1, data='dataset2')
-    generate_learning_curves(tuned_boosted_tree_model_wine, 
+    generate_learning_curves(tuned_boosted_tree_model_wine,
                              tuned_boosted_tree_model_dataset2,
                              X_train1, y_train1, X_train2, y_train2)
     
     tuned_knn_model_wine = knn_experiments(X_train1, y_train1, data='wine')
     tuned_knn_model_dataset2 = knn_experiments(X_train1, y_train1, data='dataset2')
-    generate_learning_curves(tuned_knn_model_wine, 
+    generate_learning_curves(tuned_knn_model_wine,
                              tuned_knn_model_dataset2,
                              X_train1, y_train1, X_train2, y_train2)
     
     tuned_neural_network_model_wine = neural_network_experiments(X_train1, y_train1, data='wine')
     tuned_neural_network_model_dataset2 = neural_network_experiments(X_train1, y_train1, data='dataset2')
-    generate_learning_curves(tuned_neural_network_model_wine, 
+    generate_learning_curves(tuned_neural_network_model_wine,
                              tuned_neural_network_model_dataset2,
                              X_train1, y_train1, X_train2, y_train2)
     
     tuned_support_vector_machine_model_wine = support_vector_machine_experiments(X_train1, y_train1, data='wine')
     tuned_support_vector_machine_model_dataset2 = support_vector_machine_experiments(X_train1, y_train1, data='dataset2')
-    generate_learning_curves(tuned_support_vector_machine_model_wine, 
+    generate_learning_curves(tuned_support_vector_machine_model_wine,
                              tuned_support_vector_machine_model_dataset2,
                              X_train1, y_train1, X_train2, y_train2)
     
-    test_score()
-    
+    result_data_wine = [['Decision Tree', 'wine', tuned_decision_tree_model_wine],
+                   ['Boosted Tree', 'wine', tuned_boosted_tree_model_wine],
+                   ['kNN', 'wine', tuned_knn_model_wine],
+                   ['Neural Network', 'wine', tuned_neural_network_model_wine],
+                   ['SVM', 'wine', tuned_support_vector_machine_model_wine]]
+
+    result_data_2 = [['Decision Tree', 'dataset2', tuned_decision_tree_model_dataset2],
+                   ['Boosted Tree', 'dataset2', tuned_boosted_tree_model_dataset2],
+                   ['kNN', 'dataset2', tuned_knn_model_dataset2],
+                   ['Neural Network', 'dataset2', tuned_neural_network_model_dataset2],
+                   ['SVM', 'dataset2', tuned_support_vector_machine_model_dataset2]]
+                            
+    result_df_wine = pd.DataFrame(result_data_wine, columns=['Model', 'Dataset', 'Tuned Model'])
+    result_df_2 = pd.DataFrame(result_data_2, columns=['Model', 'Dataset', 'Tuned Model'])
+    test_score(result_df_wine, X_train1, y_train1, X_test1, y_test1)
+    test_score(result_df_2, X_train2, y_train2, X_test2, y_test2)
     
     
 def generate_learning_curves(tuned_model_1, tuned_model_2, X_train1, y_train1, X_train2, y_train2):
     fig, axes = plt.subplots(3, 2, figsize=(10, 15))
 
     plot_learning_curve(tuned_model_1.best_estimator_
-                        , 'Wine Data', 
+                        , 'Wine Data',
                         X_train1, y_train1, axes=axes[:, 0], ylim=(0.7, 1.01),
                         cv=StratifiedKFold(n_splits=5, random_state=42), n_jobs=-1)
     plot_learning_curve(tuned_model_2.best_estimator_
-                        , 'Dataset2', 
+                        , 'Dataset2',
                         X_train2, y_train2, axes=axes[:, 1], ylim=(0.7, 1.01),
                         cv=StratifiedKFold(n_splits=5, random_state=42), n_jobs=-1)
     plt.show()
-    
-    
-    
-
-    
-    
 
 
-
-if __name__=='__main__':
+if __name__ == '__main__':
     main()
